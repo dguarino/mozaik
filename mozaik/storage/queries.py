2327e3ec9ce5a38e3b3d48660edbeed4f6d704f5
dguarino <domenico.guarino@gmail.com>
2019-06-05 11:41:50 +0200

    workaround to provide MozaikParametrized.idd() with the required params

--- mozaik/storage/queries.py
+++ mozaik/storage/queries.py
@@ -87,17 +87,14 @@ def param_filter_query(dsv,ads_unique=False,rec_unique=False,**kwargs):
     kwargs = dict([(k,kwargs[k]) for k in kwargs.keys() if k[0:3] != 'st_'])
     
     # seg_st = [MozaikParametrized.idd(seg.annotations['stimulus']) for seg in dsv.block.segments] # DG: original
-    # seg_st = [MozaikParametrized.idd(seg.annotations['stimulus'][1:-2]+", 'name':'"+seg.annotations['sheet_name']+"'") for seg in dsv.block.segments]
-    seg_st = []
+    seg_st = [] # DG: workaround to provide MozaikParametrized.idd() with the required params
     for seg in dsv.block.segments:
-        print  seg.annotations
+        # print  seg.annotations # {'stimulus': '{"module_path":"mozaik.stimuli",}', 'sheet_name':'Inh_Layer'}
         stidd = {}
         stidd['name'] = seg.annotations['sheet_name']
         st_p = eval(seg.annotations['stimulus'])
         stidd['module_path'] = st_p['module_path']
         seg_st.append(str(stidd))
-    # seg_st = ["'module_path':'mozaik.stimuli','name':'"+seg.annotations['sheet_name']+"'" for seg in dsv.block.segments]
-    # {'stimulus': '{"module_path":"mozaik.stimuli",}', 'sheet_name':'Inh_Layer'}
     ads_st = [MozaikParametrized.idd(ads.stimulus_id) for ads in dsv.analysis_results if ads.stimulus_id != None]
     if 'sheet_name' in set(kwargs):
        if len(kwargs) == 1:
