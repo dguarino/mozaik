bf34cd2196acdbbfa583dafb917c3b9e578450ab
dguarino <domenico.guarino@gmail.com>
2019-06-18 12:50:59 +0200

    testing T2

--- mozaik/tools/mozaik_parametrized.py
+++ mozaik/tools/mozaik_parametrized.py
@@ -312,7 +312,7 @@ class MozaikParametrized(Parameterized):
         params = eval(obj)
         print params
         if not "name" in params.keys():
-            params["name"] = self.__class__.__name__
+            params["name"] = cls.__name__
         name = params.pop("name")
         module_path = params.pop("module_path")
         
