86434e48f7faa084906def504312cd981c4ef22b
dguarino <domenico.guarino@gmail.com>
2019-06-26 17:11:36 +0200

    testing analyses: filter_query access to segment annotations (x in filter_query is a string and not a MozaikParametrized object)

--- mozaik/tools/mozaik_parametrized.py
+++ mozaik/tools/mozaik_parametrized.py
@@ -306,8 +306,8 @@ class MozaikParametrized(Parameterized):
         Furthermore if given an instance of MozaikParametrized instead it will convert it into the 'Shell' object.
         """
         if isinstance(obj,MozaikParametrized):
-           return MozaikParametrized.idd(str(obj)) # DG: original
-           # return MozaikParametrized.idd( str({"name":obj.name,"module_path":obj.module_path}) ) # DG: using the obj params to explicitly set the idd
+           # return MozaikParametrized.idd(str(obj)) # DG: original
+           return MozaikParametrized.idd( str({"name":obj.name,"module_path":obj.module_path}) ) # DG: using the obj params to explicitly set the idd
         assert isinstance(obj,str), "The object passed to the idd class method is not string: %s" % (type(obj)) 
         
         params = eval(obj)
