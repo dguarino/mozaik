8f00b4da2d69515bbd428659272b6115195e8f5e
dguarino <domenico.guarino@gmail.com>
2019-06-28 11:36:26 +0200

    testing analyses: filter_query access to segment annotations (x in filter_query is a string and not a MozaikParametrized object)

--- mozaik/storage/queries.py
+++ mozaik/storage/queries.py
@@ -86,8 +86,6 @@ def param_filter_query(dsv,ads_unique=False,rec_unique=False,**kwargs):
     st_kwargs = dict([(k[3:],kwargs[k]) for k in kwargs.keys() if k[0:3] == 'st_'])
     kwargs = dict([(k,kwargs[k]) for k in kwargs.keys() if k[0:3] != 'st_'])
     
-    for seg in dsv.block.segments:
-        print "seg.annotations", seg.annotations
     # seg_st = [MozaikParametrized.idd(seg.annotations['stimulus']) for seg in dsv.block.segments] # DG: original
     seg_st = [] # DG: workaround to provide MozaikParametrized.idd() with the required params
     for seg in dsv.block.segments:
@@ -98,6 +96,7 @@ def param_filter_query(dsv,ads_unique=False,rec_unique=False,**kwargs):
         stidd['module_path'] = st_p['module_path']
         seg_st.append(MozaikParametrized.idd(str(stidd)))
     ads_st = [MozaikParametrized.idd(ads.stimulus_id) for ads in dsv.analysis_results if ads.stimulus_id != None]
+
     if 'sheet_name' in set(kwargs):
        if len(kwargs) == 1:
            # This means that there is only one 'non-stimulus' parameter sheet, and thus we need
