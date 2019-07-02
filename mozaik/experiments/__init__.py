e48b3f4ad36794c8a855d24b34c495adb5a6844a
dguarino <domenico.guarino@gmail.com>
2019-07-02 15:10:32 +0200

    queries error (how the stimulus name is stored)

--- mozaik/experiments/__init__.py
+++ mozaik/experiments/__init__.py
@@ -93,6 +93,7 @@ class Experiment(ParametrizedObject):
                 ds = self.direct_stimulation[i]
             (segments,null_segments,input_stimulus,simulator_run_time) = self.model.present_stimulus_and_record(s,ds)
             srtsum += simulator_run_time
+            print "__init__.experiments s.annotations",[s.annotations for s in segments]
             data_store.add_recording(segments,s)
             data_store.add_stimulus(input_stimulus,s)
             
